538bb03f25dd01294cfecabdecc1f9a1689579b0
VINI <lucas.vinicius4055@gmail.com>
2025-10-06 00:06:39 -0300

    0.0.8 PHT
    Notas de Atualização:
    
    - Melhora no sistema de interação(ainda pretendo melhorar ainda mais).
    
    - Melhoria básica no sistema de portais(agora é possível mudar pra onde o portal leva no inspetor).

--- PROJETO HOMEM DA TOCHA/Assets/Scripts/Interagir.cs
+++ PROJETO HOMEM DA TOCHA/Assets/Scripts/Interagir.cs
@@ -1,9 +1,20 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Interagir : MonoBehaviour
 {
     private bool podeinteragir;
+    [Header("Acão do objeto")]
+    [Tooltip("Olhe no código para ver cada ação")]
+    public int action;
+    /*
+    Todas as a ações:
+
+    0 - Destroi o objeto.
+     o resto ainda vamos fazer
+
+    */
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,7 +31,15 @@ public class Interagir : MonoBehaviour
     {
         if (podeinteragir && Input.GetKeyDown(KeyCode.E))
         {
-            Destroy(gameObject);
+            if (action == 0)
+            {
+                Destroy(gameObject);
+            }
+            
+            if (action == 3)
+            {
+                Destroy(gameObject); // apenas testei pra ver se funciona, e funcionou :)
+            }
         }
     }
 }
--- PROJETO HOMEM DA TOCHA/Assets/Scripts/MudarCena.cs
+++ PROJETO HOMEM DA TOCHA/Assets/Scripts/MudarCena.cs
@@ -3,11 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MudarCena : MonoBehaviour
 {
+
+    public int cena = 0;
+
     private void OnTriggerEnter2D(Collider2D collision) // detecta a colisão
     {
         if (collision.CompareTag("Player")) // se estiver com a tag player
         {
-            SceneManager.LoadSceneAsync(2); // carrega a cena da cidade
+            SceneManager.LoadSceneAsync(cena); // carrega a cena da cidade
         }
     }
 }
